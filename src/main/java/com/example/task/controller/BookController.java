4137d2e6a70c415c2597af3900914f17a0aa5b15
PhantomANaz <u1704069@student.cuet.ac.bd>
2023-11-02 18:41:39 +0600

    application properties

---
+++ src/main/java/com/example/task/controller/BookController.java
@@ -0,0 +1,14 @@
+package com.example.task.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+public class BookController {
+    @GetMapping("/")
+    public String home() {
+        return "home";
+
+    }
+
+}
