00fa3bb23d8b0883749e0dd18674e9e4daef67b1
Ayesha Naznin <u1704069@student.cuet.ac.bd>
2023-11-02 23:28:20 +0600

    Add files via upload

---
+++ task/src/main/java/com/example/task/TaskApplication.java
@@ -0,0 +1,18 @@
+package com.example.task;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+//@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+
+public class TaskApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(TaskApplication.class, args);
+
+    }
+
+}
---
+++ task/src/main/java/com/example/task/controller/BookController.java
@@ -0,0 +1,21 @@
+package com.example.task.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+public class BookController {
+    @GetMapping("/")
+    public String home() {
+        return "home";
+
+    }
+    @GetMapping("/book_register")
+    public String bookRegister() {
+        return "bookRegister";
+    }
+    @GetMapping("/available_books")
+    public String getAllBook() {
+        return "bookList";
+    }
+}
